62bc87a2269905e853d41a45332dd64189b2a38d
areiks <areiks2@gmail.com>
2012-01-21 20:40:58 +0100

    itemtype

---
+++ AddItemTypeDialog.Designer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 2012-01-21
+ * Time: 20:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Lenders
+{
+	partial class AddItemTypeDialog
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(24, 69);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(135, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.button1.Location = new System.Drawing.Point(109, 121);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(66, 31);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(22, 121);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(63, 31);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Cancel";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(24, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(135, 30);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Please enter new item type";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// AddItemTypeDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(198, 189);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "AddItemTypeDialog";
+			this.Text = "Add Item Type";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		public System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ AddItemTypeDialog.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 2012-01-21
+ * Time: 20:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lenders
+{
+	/// <summary>
+	/// Description of AddItemTypeDialog.
+	/// </summary>
+	public partial class AddItemTypeDialog : Form
+	{
+		public AddItemTypeDialog()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
