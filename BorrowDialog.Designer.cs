4221b97049d29870bbd1a1a2ced0502e8e842805
areiks <areiks2@gmail.com>
2012-01-22 03:42:06 +0100

    Dodano borrow dialog

---
+++ BorrowDialog.Designer.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 2012-01-22
+ * Time: 03:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Lenders
+{
+	partial class BorrowDialog
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(0, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(411, 21);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Please choose a person who borrowed this object or click unborrow to change statu" +
+			"s.";
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+									this.columnHeader1,
+									this.columnHeader2,
+									this.columnHeader3,
+									this.columnHeader4,
+									this.columnHeader5});
+			this.listView1.Location = new System.Drawing.Point(0, 32);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(531, 199);
+			this.listView1.TabIndex = 2;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1SelectedIndexChanged);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "First Name";
+			this.columnHeader1.Width = 103;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Last Name";
+			this.columnHeader2.Width = 93;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Date of birth";
+			this.columnHeader3.Width = 109;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "City";
+			this.columnHeader4.Width = 114;
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "Address";
+			this.columnHeader5.Width = 96;
+			// 
+			// button1
+			// 
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.button1.Location = new System.Drawing.Point(316, 285);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Borrow";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.DialogResult = System.Windows.Forms.DialogResult.Yes;
+			this.button2.Location = new System.Drawing.Point(224, 285);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Unborrow";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button3.Location = new System.Drawing.Point(121, 285);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(83, 23);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Cancel";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(176, 247);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(99, 251);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(71, 23);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Borrow Date";
+			// 
+			// BorrowDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(519, 332);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.label1);
+			this.Name = "BorrowDialog";
+			this.Text = "Borrow/Unborrow";
+			this.Load += new System.EventHandler(this.BorrowDialogLoad);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label2;
+		public System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		public System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ BorrowDialog.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 2012-01-22
+ * Time: 03:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Db4objects;
+using Db4objects.Db4o.Linq;
+using System.Linq;
+using System.Collections.Generic;
+namespace Lenders
+{
+	/// <summary>
+	/// Description of BorrowDialogcs.
+	/// </summary>
+	public partial class BorrowDialog : Form
+	{
+		public BorrowDialog()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void ListView1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void BorrowDialogLoad(object sender, EventArgs e)
+		{
+				IEnumerable<Lender> lenders = from Lender l in DBConnection.Instance.DB select l;
+				foreach(Lender lender in lenders)
+				{
+					ListViewItem lvItem  = new ListViewItem(lender.FirstName);
+					lvItem.SubItems.Add(lender.LastName);
+					lvItem.SubItems.Add(lender.BirthDate.ToString());
+					lvItem.SubItems.Add(lender.City);
+					lvItem.SubItems.Add(lender.Address);
+					listView1.Items.Add(lvItem); 
+				}
+		}
+	}
+}
