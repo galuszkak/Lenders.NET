2bf65e3770331908b88e81ed5869954f61ee42f0
Kamil <galuszkak@gmail.com>
2012-01-22 12:04:16 +0100

    remove items and remove python MainForm.py

--- ManageItemDialog.Designer.cs
+++ ManageItemDialog.Designer.cs
@@ -164,6 +164,7 @@ namespace Lenders
 			// 
 			// button1
 			// 
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.No;
 			this.button1.Location = new System.Drawing.Point(93, 211);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
